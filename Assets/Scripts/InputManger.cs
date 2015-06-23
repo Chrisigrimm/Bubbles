6785330b2a549042e18b6bb6c3646a19b3e1cf94
Christoph Grimm <Christoph.Grimm@hotmail.de>
2015-06-23 18:11:27 +0200

    init

---
+++ Assets/Scripts/InputManger.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManger : MonoBehaviour
+{
+    #region Variables
+    #region Public
+    public Player player;
+    public float minSpeed = 1;
+    public float maxSpeed = 5;
+    public float radiusMouse = 2;
+    #endregion
+    #region Private
+    #endregion
+    #endregion
+
+    #region Unity-Events
+    void Update()
+    {
+        control();
+    }
+    #endregion
+
+    #region Methods
+    #region Public
+    #endregion
+
+    #region Private
+    private void control()
+    {
+        playerControl();
+    }
+
+    private void playerControl()
+    {
+        Vector2 mousePos = player.playerCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerPos = player.transform.position;
+        Vector2 differenceVector = mousePos - playerPos;
+
+        float speed = 0;
+
+        if (differenceVector.sqrMagnitude > radiusMouse * radiusMouse)
+        {
+            speed = maxSpeed;
+        }
+        else
+        {
+            if ((maxSpeed * (differenceVector.sqrMagnitude / (radiusMouse * radiusMouse))) < minSpeed)
+            {
+                speed = minSpeed;
+            }
+            else
+            {
+                speed = maxSpeed * (differenceVector.sqrMagnitude / (radiusMouse * radiusMouse));
+            }
+        }
+
+        player.movePlayer(differenceVector, speed);
+    }
+    #endregion
+    #endregion
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    #region Variables
+    public Camera playerCamera;
+    #region Public
+    #endregion
+    #region Private
+    private Rigidbody rigBody;
+    private bool test = false;
+    #endregion
+    #endregion
+
+    #region Unity-Events
+    void OnCollisionEnter(Collision collision)
+    {
+        test = true;
+    }
+
+    void Awake()
+    {
+        rigBody = GetComponent<Rigidbody>();
+    }
+    #endregion
+
+    #region Methods
+    #region Public
+    public void movePlayer(Vector2 toPosition, float speed)
+    {
+        if (!test)
+        {
+            rigBody.velocity = toPosition * (speed / rigBody.mass);
+        }
+        else
+        {
+            test = false;
+        }
+    }
+    #endregion
+
+    #region Private
+    #endregion
+    #endregion
+}
